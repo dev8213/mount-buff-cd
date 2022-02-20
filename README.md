0310a0c95f71899464c496879e3c255bd256f498
dev8213 <budisatria8213@yahoo.com>
2022-02-20 17:56:55 +0700

    custom

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 show cooldown of crit mount (1.5/2.0) on manage effect bar
+
+## Commands
+Toolbox(/8) | Command description
+--- | ---
+**buff** | Module on/off (default on).
+**buff am (abn id)** | Custom skill icon for ancient mighty cooldown buff (default off).
+**buff am reset** | Default skill icon for ancient mighty cooldown buff.
+**buff m (abn id)** | Custom skill icon for mighty cooldown buff.
+**buff m reset** | Default skill icon for mighty cooldown buff.
+**buff reload** | Reload lib data.
--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const dec = [7000,9000,11000,15000,20000]
 const inc = [1000,1200,1500,2000,3000]
 module.exports = function mountcrit(mod) {
 	
-	let mounts = reloadJS('./lib/mounts.js'),
+	function get_config() {
+		const def = {
+			"enabled": true,
+			"am": 0,
+			"m": 0
+		};
+
+		try {
+			return Object.assign({}, def, reload("./config"));
+		}catch(e) {
+			return def
+		}
+	}
+	
+	let mounts = reload('./lib/mounts.js'),
 		sup = [], drt = 0, cdr = 0,
-		enabled = true
+		config = get_config(),
+		enabled = true,
+		custom = false
 	
 	mod.game.initialize('inventory');
 	//5160215 5160216 5160217 5160218 5160219
@@ -22,19 +38,50 @@ module.exports = function mountcrit(mod) {
    })
 	
 	mod.hook('S_ABNORMALITY_BEGIN', '*', (event) => {
-		if (event.target != mod.game.me.gameId) return
-		let mount = mounts.find(obj => obj.id == event.id)
-		if (mount) mod.setTimeout(abn,Number(event.duration)+50,mount.icon,mount.cd-Number(event.duration)-cdr-100)
+		if (config.enabled && event.target == mod.game.me.gameId) {
+			let mount = mounts.find(obj => obj.id == event.id)
+		if (mount) mod.setTimeout(abn,Number(event.duration)+50,icon(mount),mount.cd-Number(event.duration)-cdr-100)
+		}
 	})
 
-	mod.command.add('buff', (p1)=> {
+	mod.command.add('buff', (p1,p2)=> {
 		if (p1) p1 = p1.toLowerCase()
+		if (p2) p2 = p2.toLowerCase()
 		if (p1 == null) {
-			enabled = !enabled
-			mod.command.message('is ' + (enabled ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')))
+			config.enabled = !config.enabled
+			mod.command.message('is ' + (config.enabled ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')))
+			JSONsave(config)
 		} else if (p1 == 'reload') {
-			mounts = reloadJS('./lib/mounts.js')
+			mounts = reload('./lib/mounts.js')
 			mod.command.message('data has been reloaded')
+		} else if (p1 == 'am') {
+			if (p2 == 'reset') {
+				config.am = 0
+				mod.command.message('ancient mighty using default icon now')
+				JSONsave(config)
+				return
+			}
+			if (isNaN(p2)) {
+				mod.command.message('arg must be a number')
+			} else {
+				config.am = p2
+				mod.command.message('custom icon for ancient mighty set to: '+p2)
+				JSONsave(config)
+			}
+		} else if (p1 == 'm') {
+			if (p2 == 'reset') {
+				config.am = 0
+				mod.command.message('ancient mighty using default icon now')
+				JSONsave(config)
+				return
+			}
+			if (isNaN(p2)) {
+				mod.command.message('arg must be a number')
+			} else {
+				config.am = p2
+				mod.command.message('custom icon for mighty set to: '+p2)
+				JSONsave(config)
+			}
 		}
 	})
 	
@@ -53,8 +100,22 @@ module.exports = function mountcrit(mod) {
             });
         }, duration);
     }
-	function reloadJS(fileName) {
+	function icon(mount) {
+		if (mount.type == "ancient mighty" && config.am != 0) return config.am
+		else if (mount.type == "mighty" && config.m != 0) return config.m
+		else return mount.id
+	}
+	function reload(fileName) {
 		delete require.cache[require.resolve(fileName)]
 		return require(fileName)
+	}
+	function JSONsave(obj) {
+		if (Object.keys(obj).length) {
+			try { fs.writeFileSync(path.join(__dirname,`./config.json`), JSON.stringify(obj, null, '\t')) }
+			catch (err) { console.log(err); return false }
+		}
+	}
+	this.destructor = function() {
+		JSONsave(config)
 	}
 }
