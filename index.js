dc8b2c133c0a83399d3433d2021fa2d7fd86b6b2
dev8213 <budisatria8213@yahoo.com>
2022-02-28 18:34:02 +0700

    a

--- index.js
+++ index.js
@@ -31,14 +31,13 @@ module.exports = function buffcd(mod) {
 	
 	mod.game.initialize('inventory');
 	mod.game.inventory.on('update', () => {
-        try { sup = mod.game.inventory.equipment.slots['4'].passivitySets[0].passivities } catch (e) { sup = [], drt = 0, cdr = 0 }
+		try { sup = mod.game.inventory.equipment.slots['4'].passivitySets[0].passivities } catch (e) { sup = [], drt = 0, cdr = 0 }
 		if (sup.length == 0) return
         if (sup[0] >= 5160215 && sup[0] <= 5160219) {
 			drt = inc[(sup[0]+5)%10]
 			cdr = dec[(sup[0]+5)%10]
-		}
-		else drt = cdr = 0
-   })
+		} else drt = cdr = 0
+	})
 	
 	mod.hook('S_ABNORMALITY_BEGIN', '*', (event) => {
 		if (!config.enabled) return
